bdb04a4a886e116dac19353f4b251478c0d0395a
Kinga <zawarczynska.ko@gmail.com>
2020-01-27 11:51:00 +0100

    current filter displayed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CardComponent from './CardComponent'
 
 const data = [
   {
-    imgSrc: 'zadanie/27283-2.jpg',
+    imgSrc: './zadanie/27283-2.jpg',
     content: 'Zagrożenie: Zatrucie. Wyrób nie spełnia wymagań § 5 ust. 1 rozporządzenia Ministra Rozwoju i Finansów z dnia 20 października 2016 r. w sprawie wymagań dla zabawek (Dz.U. z 2019, poz. 1816) oraz pkt 3 ,,Wymagania w zakresie właściwości chemicznych’’ ppkt 3.1.1 i ppkt 3.1.2 załącznika nr 1 do rozporządzenia, z uwagi na obecność w wyrobie ftalanu bis (2-etyloheksylu) (DEHP) w ilości (27,2% ± 4,4%) - dopuszczalna ilość nie może przekraczać 0,1%. Cztery miękkie, żółte kaczuszki różnej wielkości, wykonane z PCV',
     title: 'Kaczuszki do kąpieli żółte',
     country: 'Polska',
@@ -27,7 +27,7 @@ const data = [
   },
   {
     imgSrc: 'zadanie/3.jpg',
-    content: 'Przedsiębiorca ALMOT Mikołaj Sibora Sp.k. powiadomił Prezesa Urzędu Ochrony Konkurencji i Konsumentów, że wyposażenie akcesoryjne - Asystent Zmiany Biegów Triumph (A9930469), dopasowane do motocykli marki Triumph, modele: SPEED TRIPLE Z SERI NN5 i NN6, może wywołać nieprawidłowe wzajemne położenie elementów mechanizmu zmiany biegów. Jeśli ten stan wystąpi i pozostanie niewykryty, może to prowadzić do niedokładnej zmiany biegów (tylko zmiana biegu na wyższy – nie dotyczy zmiany biegów na niższy), co prowadzi do sporadycznych, fałszywych wskazań biegu neutralnego, co z kolei zwiększa ryzyko dla bezpieczeństwa kierującego.' ,
+    content: 'Przedsiębiorca ALMOT Mikołaj Sibora Sp.k. powiadomił Prezesa Urzędu Ochrony Konkurencji i Konsumentów, że wyposażenie akcesoryjne - Asystent Zmiany Biegów Triumph (A9930469), dopasowane do motocykli marki Triumph, modele: SPEED TRIPLE Z SERI NN5 i NN6, może wywołać nieprawidłowe wzajemne położenie elementów mechanizmu zmiany biegów. Jeśli ten stan wystąpi i pozostanie niewykryty, może to prowadzić do niedokładnej zmiany biegów (tylko zmiana biegu na wyższy – nie dotyczy zmiany biegów na niższy), co prowadzi do sporadycznych, fałszywych wskazań biegu neutralnego, co z kolei zwiększa ryzyko dla bezpieczeństwa kierującego.',
     title: 'Wyposażenie akcesoryjne Asystent Zmiany Biegów Triumph',
     country: 'Polska',
     tag: 'Zabawki',
@@ -38,59 +38,64 @@ const data = [
 
 
 function App() {
-  const [active, setActive] = useState('active2')
-  const [notActive, setNotActive] = useState('')
+  const [current, setCurrent] = useState('Wszystkie')
   let filteredData;
   const [filtered, setFiltered] = useState(data)
- 
+
 
   return (
     <div className="App">
       <Page>
         <Toolbar>
-          <div className='center'>Monite</div></Toolbar> 
-          
+          <div className='center'>Monite</div>
+        </Toolbar>
         <div className='flex'>
-        <h1>Najnowsze alerty</h1>
-        <Switch checked={false} onChange={() => { }}></Switch>
+          <h1>Najnowsze alerty</h1>
+          <Switch checked={false} onChange={() => { }}>
+          </Switch>
         </div>
-
-        
         {filtered.map(data =>
-        <CardComponent title={data.title} content={data.content} imgSrc={data.imgSrc} country={data.country} tag={data.tag} link={data.link} time={data.time}/>)}
-
-
-
+          <CardComponent title={data.title} content={data.content} imgSrc={data.imgSrc} country={data.country} tag={data.tag} link={data.link} time={data.time} />)}
         <BottomToolbar className='carouselContainer'>
           <Carousel fullscreen swipeable itemWidth={1200} >
             <CarouselItem swipeable>
-              <Button className='center buttons carousel'  modifier='outline' onClick={() => {filteredData = data.filter(data => data.tag === 'Wszystkie')
-            setFiltered(data)
-            setActive('active2')}}  >Wszystkie</Button>
-              <Button className='center buttons carousel' modifier='outline' onClick={() => {filteredData = data.filter(data => data.tag === 'Sprzęt elektroniczny')
-            setFiltered(filteredData)
-            setActive('active2')}}  >Sprzęt elektroniczny</Button>
-              <Button className='center buttons carousel' className={active} modifier='outline' onClick={() => {filteredData = data.filter(data => data.tag === 'Zabawki')
-            setFiltered(filteredData)
-            setActive('active2')}} >Zabawki</Button>
-              <Button className='center buttons carousel' modifier='outline' onClick={() => {filteredData = data.filter(data => data.tag === 'Motoryzacja')
-            setFiltered(filteredData)
-            setActive('active2')}} >Motoryzacja</Button>
-              <Button className='center buttons carousel' modifier='outline' onClick={() => {filteredData = data.filter(data => data.tag === 'Żywność')
-            setFiltered(filteredData)
-            setActive('active2')}} >Żywność</Button>
-              <Button className='center buttons carousel' modifier='outline' onClick={() => {filteredData = data.filter(data => data.tag === 'Nieżywność')
-            setFiltered(filteredData)
-            setActive('active2')}} >Nieżywność</Button>
+              <Button  modifier='outline' className={'Wszystkie' === current ? 'active2 center buttons carousel' : 'notActive center buttons carousel'} onClick={() => {
+                filteredData = data.filter(data => data.tag === 'Wszystkie')
+                setFiltered(data)
+                setCurrent('Wszystkie')
+              }}  >Wszystkie</Button>
+              <Button  modifier='outline' className={'Sprzęt elektroniczny' === current ? 'active2 center buttons carousel' : 'notActive center buttons carousel'} onClick={() => {
+                filteredData = data.filter(data => data.tag === 'Sprzęt elektroniczny')
+                setFiltered(filteredData)
+                setCurrent('Sprzęt elektroniczny')
+              }}  >Sprzęt elektroniczny</Button>
+              <Button className={'Zabawki' === current ? 'active2 center buttons carousel' : 'notActive center buttons carousel'} modifier='outline' onClick={() => {
+                filteredData = data.filter(data => data.tag === 'Zabawki')
+                setFiltered(filteredData)
+                setCurrent('Zabawki')
+              }} >Zabawki</Button>
+              <Button  className={'Motoryzacja' === current ? 'active2 center buttons carousel' : 'notActive center buttons carousel'} modifier='outline' onClick={() => {
+                filteredData = data.filter(data => data.tag === 'Motoryzacja')
+                setFiltered(filteredData)
+                setCurrent('Motoryzacja')
+              }} >Motoryzacja</Button>
+              <Button  modifier='outline' className={'Żywność' === current ? 'active2 center buttons carousel' : 'notActive center buttons carousel'} onClick={() => {
+                filteredData = data.filter(data => data.tag === 'Żywność')
+                setFiltered(filteredData)
+                setCurrent('Żywność')
+              }} >Żywność</Button>
+              <Button modifier='outline' className={'Nieżwyność' === current ? 'active2 center buttons carousel' : 'notActive center buttons carousel'} onClick={() => {
+                filteredData = data.filter(data => data.tag === 'Nieżywność')
+                setFiltered(filteredData)
+                setCurrent('Nieżywność')
+              }} >Nieżywność</Button>
 
             </CarouselItem>
-
-
           </Carousel>
         </BottomToolbar>
       </Page>
     </div>
   );
-        }
+}
 
 export default App;
--- src/CardComponent.js
+++ src/CardComponent.js
@@ -2,33 +2,35 @@ import React from 'react'
 import { Card } from 'react-onsenui';
 
 function CardComponent(props) {
-  const {title, content, imgSrc, country, tag, link, time} = props;
-    return (
-       
-<div >
-          <Card>
-            <div className='img'>
-              <img src={imgSrc} alt="desc"></img>
-            </div>
-            <div>
-              <div className='title'>{title} </div>
-              <div className='content'>{content}</div>
-            </div>
+  const { title, content, imgSrc, country, tag, link, time } = props;
+  return (
 
-          </Card>
-          <div className='flexIcons'> <div className='icons'>
-            <div className='flexColumn'>
-              <span className="lnr lnr-map-marker">{country}</span>
-              <span className="lnr lnr-tag">{tag}</span>
-            </div>
-
-            <span className="lnr lnr-link">{link}</span></div>
-            {{time} ? <span className="lnr lnr-clock time">{time}</span> : null}
-            </div>
-         
-
+    <div >
+      <Card>
+        <div className='img'>
+          <img src={imgSrc} alt="desc"></img>
         </div>
-    )
+        <div>
+          <div className='title'>{title} </div>
+          <div className='content'>{content}</div>
+        </div>
+
+      </Card>
+      <div className='flexIcons'>
+        <div className='icons'>
+          <div className='flexColumn'>
+            <div>
+            <span className="lnr lnr-map-marker">{country}</span>
+            <span className="lnr lnr-link">{link}</span></div>
+          </div>
+          <div><span className="lnr lnr-tag">{tag}</span></div>
+          </div>
+        {{ time } ? <span className="lnr lnr-clock time">{time}</span> : null}
+      </div>
+
+
+    </div>
+  )
 }
 
 export default CardComponent
