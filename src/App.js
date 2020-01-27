c7b29476ee6051668e6708aca1f5872cb71c7f00
Kinga <zawarczynska.ko@gmail.com>
2020-01-27 09:50:16 +0100

    displaying data in cardComponent

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import { Page, Toolbar, Button, BottomToolbar, Carousel, CarouselItem, Card, Switch } from 'react-onsenui';
+import { Page, Toolbar, Button, BottomToolbar, Carousel, CarouselItem, Switch } from 'react-onsenui';
 import 'onsenui/css/onsenui.css';
 import 'onsenui/css/onsen-css-components.css';
 import './App.css';
+import CardComponent from './CardComponent'
 
-const data = {
-  element1: {
-    imgSrc: 'zadanie/3.jpg',
+const data = [
+  {
+    imgSrc: 'zadanie/27283-2.jpg',
     content: 'Zagrożenie: Zatrucie. Wyrób nie spełnia wymagań § 5 ust. 1 rozporządzenia Ministra Rozwoju i Finansów z dnia 20 października 2016 r. w sprawie wymagań dla zabawek (Dz.U. z 2019, poz. 1816) oraz pkt 3 ,,Wymagania w zakresie właściwości chemicznych’’ ppkt 3.1.1 i ppkt 3.1.2 załącznika nr 1 do rozporządzenia, z uwagi na obecność w wyrobie ftalanu bis (2-etyloheksylu) (DEHP) w ilości (27,2% ± 4,4%) - dopuszczalna ilość nie może przekraczać 0,1%. Cztery miękkie, żółte kaczuszki różnej wielkości, wykonane z PCV',
-    title: 'Kaczuszki do kąpieli żółte'
+    title: 'Kaczuszki do kąpieli żółte',
+    country: 'Polska',
+    tag: 'Zabawki',
+    link: 'UOKiK'
   },
-  element2: {
-    imgSrc: 'zadanie/3.jpg',
-    content: 'Zagrożenie: Zatrucie. Wyrób nie spełnia wymagań § 5 ust. 1 rozporządzenia Ministra Rozwoju i Finansów z dnia 20 października 2016 r. w sprawie wymagań dla zabawek (Dz.U. z 2019, poz. 1816) oraz pkt 3 ,,Wymagania w zakresie właściwości chemicznych’’ ppkt 3.1.1 i ppkt 3.1.2 załącznika nr 1 do rozporządzenia, z uwagi na obecność w wyrobie ftalanu bis (2-etyloheksylu) (DEHP) w ilości (27,2% ± 4,4%) - dopuszczalna ilość nie może przekraczać 0,1%. Cztery miękkie, żółte kaczuszki różnej wielkości, wykonane z PCV',
-    title: 'Kaczuszki do kąpieli żółte'
+  {
+    imgSrc: 'zadanie/27320-1.jpg',
+    content: 'Zagrożenie: Uduszenie. Wyrób nie spełnia wymagań § 5 ust. 1 i ust. 2 rozporządzenia Ministra Rozwoju i Finansów z dnia 20 października 2016 r. w sprawie wymagań dla zabawek (Dz.U. poz. 1730) oraz w związku z ppkt 1.4.2 i ppkt 1.4.3 załącznika nr 1 pkt 1 „Wymagania w zakresie właściwości fizycznych i mechanicznych” do rozporządzenia, z uwagi na odłączenie od zabawki przyssawki z linką, która jest jednocześnie małym elementem. Ponadto na wyrobie nie umieszczono oznakowania CE',
+    title: 'Kot maskotka miękka wypchana',
+    country: 'Polska',
+    tag: 'Zabawki',
+    link: 'UOKiK',
+    time: '1 godz.'
   },
-  element3: {
+  {
     imgSrc: 'zadanie/3.jpg',
-    content: 'Zagrożenie: Zatrucie. Wyrób nie spełnia wymagań § 5 ust. 1 rozporządzenia Ministra Rozwoju i Finansów z dnia 20 października 2016 r. w sprawie wymagań dla zabawek (Dz.U. z 2019, poz. 1816) oraz pkt 3 ,,Wymagania w zakresie właściwości chemicznych’’ ppkt 3.1.1 i ppkt 3.1.2 załącznika nr 1 do rozporządzenia, z uwagi na obecność w wyrobie ftalanu bis (2-etyloheksylu) (DEHP) w ilości (27,2% ± 4,4%) - dopuszczalna ilość nie może przekraczać 0,1%. Cztery miękkie, żółte kaczuszki różnej wielkości, wykonane z PCV',
-    title: 'Kaczuszki do kąpieli żółte'
+    content: 'Przedsiębiorca ALMOT Mikołaj Sibora Sp.k. powiadomił Prezesa Urzędu Ochrony Konkurencji i Konsumentów, że wyposażenie akcesoryjne - Asystent Zmiany Biegów Triumph (A9930469), dopasowane do motocykli marki Triumph, modele: SPEED TRIPLE Z SERI NN5 i NN6, może wywołać nieprawidłowe wzajemne położenie elementów mechanizmu zmiany biegów. Jeśli ten stan wystąpi i pozostanie niewykryty, może to prowadzić do niedokładnej zmiany biegów (tylko zmiana biegu na wyższy – nie dotyczy zmiany biegów na niższy), co prowadzi do sporadycznych, fałszywych wskazań biegu neutralnego, co z kolei zwiększa ryzyko dla bezpieczeństwa kierującego.' ,
+    title: 'Wyposażenie akcesoryjne Asystent Zmiany Biegów Triumph',
+    country: 'Polska',
+    tag: 'Zabawki',
+    link: 'UOKiK'
   }
-}
+]
+
 
 function App() {
 
@@ -28,30 +40,20 @@ function App() {
     <div className="App">
       <Page>
         <Toolbar>
-          <div className='center'>Monite</div></Toolbar>
-          <Switch checked={'checked'} onChange={() => {}}></Switch>} />
-        <Card>
-          <div className='img'>
-            <img src="zadanie/27283-2.jpg" alt="desc"></img>
-
-          </div>
-          <div>
-            <div className='title'>Kaczuszki do kąpieli żółte</div>
-            <div className='content'>Zagrożenie: Zatrucie. Wyrób nie spełnia wymagań § 5 ust. 1 rozporządzenia Ministra Rozwoju i Finansów z dnia 20 października 2016 r. w sprawie wymagań dla zabawek (Dz.U. z 2019, poz. 1816) oraz pkt 3 ,,Wymagania w zakresie właściwości chemicznych’’ ppkt 3.1.1 i ppkt 3.1.2 załącznika nr 1 do rozporządzenia, z uwagi na obecność w wyrobie ftalanu bis (2-etyloheksylu) (DEHP) w ilości (27,2% ± 4,4%) - dopuszczalna ilość nie może przekraczać 0,1%. Cztery miękkie, żółte kaczuszki różnej wielkości, wykonane z PCV</div>
-          </div>
+          <div className='center'>Monite</div></Toolbar> 
           
-        </Card>
-        <div className='icons'>
-          <div className='flexColumn'>
-          <span class="lnr lnr-map-marker">Polska</span>
-        <span class="lnr lnr-tag">Zabawki</span>
-          </div>
-        
-        <span class="lnr lnr-link">UOKiK</span></div>
-        
+        <div className='flex'>
+        <h1>Najnowsze alerty</h1>
+        <Switch checked={false} onChange={() => { }}></Switch>
+        </div>
+
+        {data.map(data =>
+        <CardComponent title={data.title} content={data.content} imgSrc={data.imgSrc} country={data.country} tag={data.tag} link={data.link} time={data.time}/>)}
+
+
 
         <BottomToolbar className='carouselContainer'>
-          <Carousel fullscreen swipeable itemWidth={1200} animationOptions={"duration: 0.2, delay: 0.4, timing: 'ease-in'"} >
+          <Carousel fullscreen swipeable itemWidth={1200} >
             <CarouselItem swipeable>
               <Button className='center buttons carousel' modifier='outline' >Wszystkie</Button>
               <Button className='center buttons carousel' modifier='outline' >Sprzęt elektroniczny</Button>
@@ -68,6 +70,6 @@ function App() {
       </Page>
     </div>
   );
-}
+        }
 
 export default App;
---
+++ src/CardComponent.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Card } from 'react-onsenui';
+
+function CardComponent(props) {
+  const {title, content, imgSrc, country, tag, link, time} = props;
+    return (
+       
+<div >
+          <Card>
+            <div className='img'>
+              <img src={imgSrc} alt="desc"></img>
+            </div>
+            <div>
+              <div className='title'>{title} </div>
+              <div className='content'>{content}</div>
+            </div>
+
+          </Card>
+          <div className='flexIcons'> <div className='icons'>
+            <div className='flexColumn'>
+              <span className="lnr lnr-map-marker">{country}</span>
+              <span className="lnr lnr-tag">{tag}</span>
+            </div>
+
+            <span className="lnr lnr-link">{link}</span></div>
+            {{time} ? <span className="lnr lnr-clock time">{time}</span> : null}
+            </div>
+         
+
+        </div>
+    )
+}
+
+export default CardComponent
+
+
+
